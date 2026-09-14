80d5ac2c0fc5a66e3e8e116085ed3192fbaa558b
sookyboo <sookyboo@gmail.com>
2026-09-14 21:38:22 +0100

    wc3: preserve stale handle save semantics

--- games/warcraft-3/game/g_local.h
+++ games/warcraft-3/game/g_local.h
@@ -2188,6 +2188,7 @@ void T_Damage(LPEDICT, LPEDICT, int);
 // g_utils.c
 void G_FreeEdict(LPEDICT);
 void G_DeferFreeEdict(LPEDICT);
+BOOL G_IsDeferredFree(LPCEDICT);
 void G_RunDeferredFrees(void);
 void G_ResetDeferredFrees(void);
 LPEVENT G_MakeEvent(EVENTTYPE);
